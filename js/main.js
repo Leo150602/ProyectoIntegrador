c0499e136fa3e10477c8f10ba6137963e1baddb4
kevinLeonadoGamboa <kevingamboamosquera@gmail.com>
2025-07-22 06:10:04 -0500

    creacion de apartado de paneles de iagenes

--- js/main.js
+++ js/main.js
@@ -43,3 +43,36 @@ btnAdelante.addEventListener("click", () => {
 })
 
 //fin del carrusel------------------------------------------
+// creacion de paneles -------------------------------------
+
+const crearPanel = (imagenes,links, contenedorId) =>{
+    const panel = document.createElement("div")
+    const contenedor = document.getElementById(contenedorId)
+    if(imagenes.length == 1){
+        contenedor.className = "panel1x1"
+    } else if(imagenes.length == 2){
+        contenedor.className = "panel2x1"
+    } else if(imagenes.length == 3){
+        contenedor.className = "panel3x1"
+    } else if(imagenes.length == 4){
+        contenedor.className = "panel4x1"
+    }
+
+    for (let index = 0; index < imagenes.length; index++) {
+
+        const link = document.createElement("a")
+        link.href = links[index]
+        
+        const imagen = document.createElement("img")
+        imagen.src = imagenes[index]
+
+        contenedor.appendChild(link)
+        link.appendChild(imagen)
+    }
+    
+}
+//         necesita ponerle el link de las paginas
+crearPanel(['imagenes/imagenHombre.webp','imagenes/imagenMujer.webp'],["about:blank","about:blank"],"hombreMujer")
+crearPanel(["imagenes/imagenNewDrop.webp"],["about:blank"],"newDropLink")
+
+// fin de la creacion de paneles ------------------------------------------
